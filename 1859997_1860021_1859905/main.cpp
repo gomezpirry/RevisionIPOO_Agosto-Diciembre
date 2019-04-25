29c89f00772aed771d2612c70f2a7f295ddf77fb
bustosgaviria <48114035+bustosgaviria@users.noreply.github.com>
2019-04-24 23:39:59 -0500

    Create main.cpp

---
+++ 1859997_1860021_1859905/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "Articulo.h"
+#include "Fecha.h"
+#include "Computo.h"
+#include "Mueble.h"
+#include "Prestamo.h"
+#include "Archivo.h"
+#include "Menu.h"
+#include <vector>
+
+
+
+
+using namespace std;
+
+
+int main() {
+
+	 Menu menuConsulta;
+
+	menuConsulta.menuPrincipal();
+
+}
